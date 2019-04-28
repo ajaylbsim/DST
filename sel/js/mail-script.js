614a7dfa51f817942a5aba292dcb052025affaaf
Ajay mishra <ajay3.mishra@airtel.com>
2019-04-29 02:05:01 +0530

    add

--- sel/js/mail-script.js
+++ sel/js/mail-script.js
@@ -1,31 +1,32 @@
     // -------   Mail Send ajax
 
+
+function myFunction() {
+	var username = $('#amount1');
+	var password = $('#amount3');
+    var usernameValue= username[0].value;
+		console.log("username",password);
+
+	var passwordValue= password[0].value;
+
+//	if(("useradmin@khs" === usernameValue)&&("password@admin@khs" === passwordValue) ){
+//	}else{
+//		alert("Invalid username or password. Please try again!");
+//	}
+//	
+	
+	
+	if(("a" === usernameValue)&&("b" === passwordValue) ){
+		window.location.href = "http://www.khsbihar.com/admin.html";
+	}else{
+		alert("Invalid username or password. Please try again!");
+	}
+	
+	
+}
      $(document).ready(function() {
         var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
-        var alert = $('.alert-msg'); // alert div for show alert message
-
-        // form submit event
-        form.on('submit', function(e) {
-            e.preventDefault(); // prevent default form submit
-
-            $.ajax({
-                url: 'mail.php', // form action url
-                type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html('Sending....'); // change submit button text
-                },
-                success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
-                },
-                error: function(e) {
-                    console.log(e)
-                }
-            });
-        });
+        var submit = $('.submit-btn');
+		
+      
     });
